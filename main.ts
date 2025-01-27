bc7771c6becaf5598a15fc0400c8d0470fb55d76
Rotri Salvatore <swftcld@gmail.com>
2025-01-27 18:59:46 -0500

    Initial commit with simplified plugin

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface MyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: 'default',
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,105 +14,17 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+		// Example: Add an icon in the left ribbon
+		this.addRibbonIcon('dice', 'My Plugin', () => {
+			new Notice('Hello from your plugin!');
 		});
 
-		// Grey text command
-		this.addCommand({
-			id: 'insert-grey-text',
-			name: 'Insert grey text after cursor',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-			  // Insert text at the cursor
-			  const cursor = editor.getCursor();
-			  const greyText = '<span style="color: lightgrey;">grey text</span>';
-			  editor.replaceRange(greyText, cursor);
-			}
-		  });
-		
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Example: Add a settings tab
+		this.addSettingTab(new MyPluginSettingTab(this.app, this));
 	}
-
-	/*
-	// This function inserts text right after the cursor position in the active markdown editor
-	insertTextAfterCursor() {
-		// Get the current active Markdown view (editor)
-		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		
-		// Check if we have a valid editor
-		if (markdownView) {
-			const editor = markdownView.editor;  // Get the editor instance
-			
-			// Get the current cursor position (line and character)
-			const cursorPos = editor.getCursor();
-			
-			// The text we want to insert (in this case, light grey colored text)
-			const textToInsert = '<span style="color: lightgrey;">This is light grey text</span>';
-			
-			// Insert the text after the cursor at the current position
-			editor.replaceRange(textToInsert, cursorPos);
-		}
-	}
-	*/
 
 	onunload() {
-
+		// Optional cleanup logic when the plugin is disabled
 	}
 
 	async loadSettings() {
@@ -126,23 +36,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class MyPluginSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -151,19 +45,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Example Setting')
+			.setDesc('Enter some text to save in the plugin settings.')
+			.addText((text) =>
+				text
+					.setPlaceholder('Type here...')
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
